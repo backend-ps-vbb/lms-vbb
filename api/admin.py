df411e03c4220ebb091e2abcadffbbaf697409bf
agarwalmanas2300 <80976113+agarwalmanas2300@users.noreply.github.com>
2021-07-02 19:24:08 +0530

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Student, Author, Book, BookInstance, Notice
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['libid', 'regno', 'branch', 'section', 'semester']
+    
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['code', 'bookname', 'author', 'publication', 'subject', 'instances']
+    
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['book', 'due_back', 'student']
+    
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'about']
+    
+@admin.register(Notice)
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ['posted_on', 'title', 'content']
+    
