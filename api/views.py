f1359e1bb29a9b64460c0f7af8cf6f688223902a
agarwalmanas2300 <80976113+agarwalmanas2300@users.noreply.github.com>
2021-07-02 18:08:59 +0530

    Used ModelsViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from .models import Student, Book, BookInstance, Author, Notice
+from .serializers import StudentSerializer, BookSerializer, AuthorSerializer, BookInstanceSerializer, NoticeSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class StudentModelViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+  
+class BookModelViewset(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class AuthorModelViewset(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookInstanceModelViewset(viewsets.ModelViewSet):
+    queryset = BookInstance.objects.all()
+    serializer_class = BookInstanceSerializer
+    
+class NoticeModelViewset(viewsets.ModelViewSet):
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
