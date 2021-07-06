9fdd1e161f97103196b2e67b2c79bc936c3d81c3
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-07-06 17:04:34 +0530

    fixed author issue

--- api/serializers.py
+++ api/serializers.py
@@ -1,28 +1,34 @@
+from django.utils import translation
 from rest_framework import serializers
 from .models import Book,BookInstance,Student,Author,Notice
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-	copies = serializers.HyperlinkedRelatedField(view_name='Book-detail',many=True, queryset=BookInstance.objects.all(),allow_null=True)
+class AuthorSerializer(serializers.ModelSerializer):
+	# books = serializers.HyperlinkedRelatedField(view_name='Author-detail',many=True, queryset=Book.objects.all(),allow_null=True)
+	class Meta:
+		model = Author
+		fields = ['id','first_name', 'last_name', 'about']
+
+
+class BookSerializer(serializers.ModelSerializer):
+	# copies = serializers.HyperlinkedRelatedField(view_name='Book-detail',many=True, queryset=BookInstance.objects.all(),allow_null=True)
+	# instance = serializers.PrimaryKeyRelatedField(many=True,queryset=BookInstance.objects.all())
 	# returns a list of hyperlinks to the book instances, can change to returning just the primary key
 	class Meta:
 		model = Book
-		fields = ['code', 'bookname', 'author','publication','subject','instances','copies']
-
-class BookInstanceSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = BookInstance
-		fields = ['id','book', 'due_back', 'student','status']
+		fields = ['code', 'bookname', 'author','publication','subject']
 
 class StudentSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Student
 		fields = ['libid','regno','branch','section','semester','yearofadm']
 
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
-	books = serializers.HyperlinkedRelatedField(view_name='Author-detail',many=True, queryset=Book.objects.all(),allow_null=True)
+class BookInstanceSerializer(serializers.ModelSerializer):
+	book = BookSerializer()
+	# student = StudentSerializer()
 	class Meta:
-		model = Author
-		fields = ['id','first_name', 'last_name', 'books', 'about']
+		model = BookInstance
+		fields = ['id','book', 'due_back', 'student','status']
+
 
 class NoticeSerializer(serializers.ModelSerializer):
 	class Meta:
