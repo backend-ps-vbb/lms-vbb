5d7bb975c2aac5d915f3eecb391bd25b9acfe615
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-07-12 15:14:48 +0530

    removed debug prints

--- lms/settings.py
+++ lms/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print('dir:')
-print(BASE_DIR)
+# print('dir:')
+# print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
