2ced3b06efefb4c905594594650629155502961d
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-07-13 18:16:27 +0530

    working on approve notices

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from api.models import Notice
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -21,15 +22,76 @@ def notice_board(request):
 # @user_passes_test(is_librarian)
 def approve_notice(request):
 	if (not (request.user.is_superuser)):
-		print("here")
+		# print("here")
 		return render(request, 'frontend/error.html',{
 			"tilte":'Unauthorized',
 			"auth_error":1,
 			"error_message":"Not Authorized to access this feature.",
 			"redirect": "/app/approvenotice"
 		})
-	print("authorized")
-	return render( request, "frontend/noticeboard.html", {
-		"notices":None,
-		"title":"Notice editing"
+	# print("authorized")
+	notices = []
+	for notice in Notice.objects.all():
+		if(not notice.is_approved):
+			notices.append(notice)
+
+	if(request.method == 'POST'):
+		try:
+			action = request.POST['action']
+		except:
+			return render( request, "frontend/approvenotice.html", {
+			"notices":notices,
+			"title":"No Action specified!"
+		})
+
+		#action is present
+		if( (not(action == "approve")) and (not(action == "delete")) ):
+			print(action + " : is invalid")
+			return render( request, "frontend/approvenotice.html", {
+				"notices":notices,
+				"title":"Incorrect Action specified!"
+			})
+
+		#Correct action is present
+		to_modify = []
+		valid = True
+		for field in request.POST:
+			# Dev note - need to modify if more form fields are added
+			print(field, request.POST[field])
+			if(field=="csrfmiddlewaretoken"):
+				continue
+			elif(field=="action"):
+				continue
+			else:
+				if(Notice.objects.filter(pk=int(field)).exists()):
+					obj = Notice.objects.get(pk=int(field))
+					if(obj.is_approved):
+						valid = False
+						break
+					print(field, obj)
+					to_modify.append(obj)
+				else:
+					valid = False
+					break
+		if (valid):
+			print("valid")
+			for notice in to_modify:
+				print(notice)
+		else:
+			print("invalid")
+			return render( request, "frontend/approvenotice.html", {
+			"notices":notices,
+			"title":"Incorrect Notice IDs selected"
+			})
+
+
+		return render( request, "frontend/approvenotice.html", {
+		"notices":notices,
+		"title":"Done"
+		})
+
+	# get request here
+	return render( request, "frontend/approvenotice.html", {
+		"notices":notices,
+		"title":"Approve Notices"
 	})
