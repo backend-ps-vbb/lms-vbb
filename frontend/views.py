4d7f1bc1b79d77541ef9c23f4092e9b6c374283d
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-07-13 13:12:06 +0530

    auth for notice approval

--- frontend/views.py
+++ frontend/views.py
@@ -5,19 +5,30 @@ from django.contrib.auth.decorators import user_passes_test
 
 def is_librarian(user):
     return (user.is_superuser )
+
 @login_required
 def notice_board(request):
 	notices = []
 	for notice in Notice.objects.all():
-		notices.append(notice)
+		if(notice.is_approved):
+			notices.append(notice)
 	return render( request, "frontend/noticeboard.html", {
 		"notices":notices,
 		"title":"noticeboard"
 	})
 
 @login_required
-@user_passes_test(is_librarian)
+# @user_passes_test(is_librarian)
 def approve_notice(request):
+	if (not (request.user.is_superuser)):
+		print("here")
+		return render(request, 'frontend/error.html',{
+			"tilte":'Unauthorized',
+			"auth_error":1,
+			"error_message":"Not Authorized to access this feature.",
+			"redirect": "/app/approvenotice"
+		})
+	print("authorized")
 	return render( request, "frontend/noticeboard.html", {
 		"notices":None,
 		"title":"Notice editing"
