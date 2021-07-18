245fc31f7a6876dee2b99b31075aadd3522c9091
tanmay-rpatil <f20190054@goa.bits-pilani.ac.in>
2021-07-18 12:35:00 +0530

    urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,13 @@ app_name="frontend"
 urlpatterns = [
     # path("", views.index, name="index"),
     # path("search", views.search, name="search"),
+    
+    # path("books", views.BookListView, name="books"),
+    # path('book/<int:pk>/',views.BookDetailView, name='detail'),
+    # path('book/create/', views.BookCreate, name='book_create'),
+    # path('book/<int:pk>/update/', views.BookUpdate, name='book_update'),
+    # path('book/<int:pk>/delete/', views.BookDelete, name='book_delete'),
+
     path("noticeboard", views.notice_board, name="notice_board"),
     path("approvenotice", views.approve_notice, name="approve_notice"),
      
